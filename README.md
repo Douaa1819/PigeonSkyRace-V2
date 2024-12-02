b54c353719137c1ee06055d8fc2f2fa185139317
Douaa <douaachemnane@gmail.com>
2024-12-02 22:39:14 +0100

    PIG-2: Configure database environment and connection settings
    - Set up PostgreSQL connection properties in application.yaml
    - Configured HikariCP connection pool for better performance.
    - Updated database URL, username, and password in the configuration.
    - Ensured compatibility with the new database instance.

--- README.md
+++
@@ -1,2 +0,0 @@
-# PigeonSkyRace-V2
-Version 2 of PigeonSky with authentication using Spring Security
---
+++ src/main/java/com/youcode/pigeonskyracev2/entity/Role.java
@@ -0,0 +1,19 @@
+package com.youcode.pigeonskyracev2.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
+
+}
